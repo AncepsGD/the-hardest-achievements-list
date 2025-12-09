36ce0a6013854b5f9e0366d9d7e7e751e433fc40
Anceps <ancspeepyalt2tg4gw@gmail.com>
2025-12-09 00:12:43 -0700

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -4,9 +4,13 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        {/* Google Fonts Comfortaa */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, minimum-scale=1, maximum-scale=5, user-scalable=yes" />
+        <meta name="theme-color" content="#23283E" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="The Hardest Achievements" />
+        
         <link href="https://fonts.googleapis.com/css?family=Comfortaa:400,700&display=swap" rel="stylesheet" />
-        {/* Bootstrap Icons CDN */}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
       </Head>
       <body>
